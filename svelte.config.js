abec637b1c3b2861de7fb04e590b88af0a8a5f1c
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-03-04 10:02:37 +0530

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: ['.svelte', '.md'],
+
+  kit: {
+    adapter: adapter(),
+
+    prerender: {
+      entries: ['*'],
+      handleMissingId: 'warn',
+    },
+
+		preprocess: vitePreprocess()
+  },
+};
+
+export default config;
